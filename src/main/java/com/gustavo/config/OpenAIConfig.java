866aa1a8d83b90c2c9a3dc1d0af35fc8e2ac1dfb
Gustavo Cruz <gu.cruz17@hotmail.com>
2024-03-02 16:07:39 -0300

    Creating the RestTemplate Bean to Consume the ChatGPT API

--- src/main/java/com/gustavo/config/OpenAIConfig.java
+++ src/main/java/com/gustavo/config/OpenAIConfig.java
@@ -3,7 +3,9 @@ package com.gustavo.config;
 import java.util.logging.Logger;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class OpenAIConfig {
@@ -12,5 +14,17 @@ public class OpenAIConfig {
 	
 	@Value("${openai.api.key}")
 	String openApiKey;
+	
+	@Bean
+	public RestTemplate template() {
+		logger.info("Initializing RestTemplate");
+		
+		RestTemplate restTemplate = new RestTemplate();
+		restTemplate.getInterceptors().add((request, body, execution) -> {
+			request.getHeaders().add("Authorization", "Bearer " + openApiKey);
+			return execution.execute(request, body);
+		});
+		return restTemplate;
+	}
 
 }
